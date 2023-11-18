3ed996f56fc7d67d0aca869bb360d7ac470c5654
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2023-11-18 22:25:33 +0530

    object types

---
+++ object.js
@@ -0,0 +1,31 @@
+let salaries={
+    John:100,
+    Sowmi:200,
+    Sindhu:300,
+};
+//dot syntax
+console.log(salaries.John);
+
+//box syntax
+let salaries={
+    John:100,
+    Sowmi:200,
+    Sindhu:300,
+};
+console.log(salaries["Sowmi"]);
+
+//object.keys
+let salaries={
+    John:100,
+    Sowmi:200,
+    Sindhu:300,
+};
+console.log(Object.keys(salaries));
+
+//Object.values
+let salaries={
+    John:100,
+    Sowmi:200,
+    Sindhu:300,
+};
+console.log(Object.values(salaries));
